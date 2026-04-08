61619ce3419c99a7c77bd61ee7152765974478b7
Piyush Kumar <piyushmvs666@gmail.com>
2026-04-08 23:51:06 +0530

    Create problem.py

---
+++ problem.py
@@ -0,0 +1,16 @@
+# Print Fibonacci series up to n terms
+
+n = int(input("Enter number of terms: "))
+
+# First two terms
+a, b = 0, 1
+
+if n <= 0:
+    print("Please enter a positive number")
+elif n == 1:
+    print(a)
+else:
+    print("Fibonacci Series:")
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
